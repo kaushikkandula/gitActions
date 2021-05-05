53d58f3e444ce3713f00066c9f4cc500bc6d8956
kaushik <kaushik.kandula@icloud.com>
2021-05-05 09:42:29 +0530

    initial commit

---
+++ src/main/java/com/example/svn2gitMigration/Svn2gitMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.svn2gitMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svn2gitMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Svn2gitMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/svn2gitMigration/Svn2gitMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.svn2gitMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Svn2gitMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
